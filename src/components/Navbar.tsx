86040972079ce233c117f5f62894d797ce56f17a
LucasArielAguirre <lucasaguirreescuela@gmail.com>
2025-05-20 21:47:06 +0200

    change bg section contact

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     animate={{ opacity: 1, y: 0 }}
     transition={{ duration: 0.5, delay: 2.2 }}
     exit={{ opacity: 0, y: -50 }}
-    className="bg-black text-white w-full p-4 flex justify-between h-12 items-center align-middle border-b decoration-white font-inter ">
+    className="fixed bg-black text-white w-full p-4 flex justify-between h-12 items-center align-middle border-b decoration-white font-inter ">
       <ul className="flex flex-row gap-4 font-italic items-center w-full justify-center">
         <li className="py-2 px-4 hover:scale-105 transition-all ">
           <a href="/">{t("home")}</a>
