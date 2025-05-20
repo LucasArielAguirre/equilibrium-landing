ea09ef318c2f8c669d20891f1f5c46c7d7d5c595
LucasArielAguirre <lucasaguirreescuela@gmail.com>
2025-05-20 21:08:07 +0200

    change image avif

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,7 @@
 
 const Contact = () => {
   return (
-    <section className="w-full h-[240px] bg-[url('/01-lan.jpg')] bg-cover text-white flex flex-col justify-center items-center font-inter border-gray-500 border-t border-b">
+    <section className="w-full h-[240px] bg-[url('/01-lan.avif')] bg-cover text-white flex flex-col justify-center items-center font-inter border-gray-500 border-t border-b">
         <div className="bg-black/50 w-full h-full flex  flex-col justify-center items-center">
             <h1 className="text-2xl font-inter">Your future in hands of FinanceTeam</h1>
             <button className="bg-black border border-white py-2 px-4 cursor-pointer rounded-xl hover:scale-105 transition-all mt-2">Contact Us</button>
